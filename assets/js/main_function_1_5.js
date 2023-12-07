86b631765c8daef840d254be66129921b805d3a7
JohannaHaer <johanna.haerdter@gmail.com>
2023-12-07 13:57:25 +0100

    function 1_5

---
+++ assets/js/main_function_1_5.js
@@ -0,0 +1,13 @@
+let num1 = 10
+let num2 = 2
+let num3 = 30
+let num4 = 20
+
+function math(number1, number2, number3, number4) {
+    console.log(`Multiplikation von ${num1} und ${num2} ist: ${number1 * number2}`);
+    console.log(`Division von ${num1} und ${num2} ist: ${number1 / number2}`);
+    console.log(`Multiplikation von ${num3} und ${num4} ist: ${number3 * number4}`);
+    console.log(`Division von ${num3} und ${num4} ist: ${number3 / number4}`);
+}
+
+math(num1, num2, num3, num4)
