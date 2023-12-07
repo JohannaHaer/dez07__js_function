60eeef711f9106d4e87d6c969ee6bce99c92ffb8
JohannaHaer <johanna.haerdter@gmail.com>
2023-12-07 17:42:34 +0100

    function 3.1

---
+++ assets/js/main_function_3_1.js
@@ -0,0 +1,36 @@
+function colorChange1() {
+    let hintergrund = document.querySelector('body');
+    let bgColor = "green";
+    hintergrund.style.backgroundColor = bgColor;
+    let h1 = document.querySelector('h1');
+    let colorH1 = "purple";
+    h1.style.color = colorH1;
+}
+
+function colorChange2() {
+    let hintergrund = document.querySelector('body');
+    let bgColor = "gray";
+    hintergrund.style.backgroundColor = bgColor;
+    let h1 = document.querySelector('h1');
+    let colorH1 = "rgb(172, 172, 2)";
+    h1.style.color = colorH1;
+}
+
+function colorChange3() {
+    let hintergrund = document.querySelector('body');
+    let bgColor = "rgb(9, 9, 53)";
+    hintergrund.style.backgroundColor = bgColor;
+    let h1 = document.querySelector('h1');
+    let colorH1 = "orange";
+    h1.style.color = colorH1;
+}
+
+function colorChange4() {
+    let hintergrund = document.querySelector('body');
+    let bgColor = "darkblue";
+    hintergrund.style.backgroundColor = bgColor;
+    let h1 = document.querySelector('h1');
+    let colorH1 = "lightblue";
+    h1.style.color = colorH1;
+}
+
